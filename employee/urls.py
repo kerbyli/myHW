4ab6fc7def9c72d15ef8ce27048b9a1cd63db065
kerbyli <kerbyli@qq.com>
2021-03-10 11:12:52 +0800

    first commit

---
+++ employee/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.emp),
+    path('emp/',views.emp),
+    path('show/',views.show),
+    path('edit/<int:id>',views.edit),
+    path('update/<int:id>',views.update),
+    path('delete/<int:id>',views.destory),
+
+]
